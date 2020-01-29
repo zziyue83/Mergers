135f9dccae4f0803ed9cf2da281525c2e4facbeb
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-28 22:58:28 -0600

    beer1.1

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -3,7 +3,7 @@ import os
 
 products_path = "../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
 products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
-beerProducts = products[products['product_group_descr'].str.contains("BEER")]
+beerProducts = products[(products['product_group_descr'].isnotnull()) & (products['product_group_descr'].str.contains("BEER"))]
 print(products.iloc[0])
 print(beerProducts.iloc[0])
 print(beerProducts.shape)
