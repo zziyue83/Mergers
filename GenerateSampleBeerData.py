2712aaf07aedb571597816b2b876061303779480
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-28 22:35:29 -0600

    access1.4

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -3,6 +3,7 @@ import os
 
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 print(str(script_dir))
+print(os.getcwd() + "\n")
 # rel_path = "/../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
 # products_path = os.path.join(script_dir, rel_path)
 # products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
