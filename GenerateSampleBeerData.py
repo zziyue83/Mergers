c6b30ac344afd9af683fbe62e3aa3b9fde39af67
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-28 22:44:55 -0600

    access1.7

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -6,7 +6,7 @@ import os
 # print(os.getcwd() + "\n")
 # rel_path = "/../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
 # products_path = os.path.join(script_dir, rel_path)
-products_path = "/../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
+products_path = "../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
 products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
 products.iloc[0]
 products.columns
