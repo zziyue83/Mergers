72935a9ff28fbbe8db8d427e388592bd6d8a9d54
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-28 22:54:45 -0600

    beer1.0

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import os
 
-# script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-# print(str(script_dir))
-# print(os.getcwd() + "\n")
-# rel_path = "/../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
-# products_path = os.path.join(script_dir, rel_path)
 products_path = "../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
 products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
+beerProducts = products[products['product_group_descr'].str.contains("BEER")]
 print(products.iloc[0])
-print(products.columns)
-print("hello")
+print(beerProducts.iloc[0])
+print(beerProducts.shape)
