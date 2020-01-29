83522f480e05ab72e843d66a330fce9945491423
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-29 12:54:24 -0600

    gb 1.1

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -32,7 +32,7 @@ for index, row in beer.iterrow():
 
 chunk_list = []
 for data_chunk in movements:
-    filtered_chunk = data_chunk[data_chunk["upc"] in beer_UPCs]
+    filtered_chunk = data_chunk[int(data_chunk["upc"]) in beer_UPCs]
     chunk_list.append(filtered_chunk)
 
 upc_movements = pd.concat(chunk_list)
