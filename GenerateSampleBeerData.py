54e06e72764e45055d10bf8d67eff321eb1aba01
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-01-28 22:23:17 -0600

    try access products.tsv

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import os
 
-print("hello world")
-print("hello again")
-print("hello thrice")
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "/../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
+products_path = os.path.join(script_dir, rel_path)
+products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
+products.iloc[0]
+products.columns
