67dd0f5077ccaaac267f907bd1c9f6d6562f7112
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-02-13 13:30:17 -0600

    fixed .sh

--- submit_example.sh
+++ submit_example.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name="[[job name]]"
+#SBATCH --job-name="[[Mergers Generate Beer Data]]"
 #SBATCH -A b1048
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -t 4:00:00
 #SBATCH -p buyin
-#SBATCH --mail-user=[[email]]
+#SBATCH --mail-user=[[yintianzhan2021@u.northwestern.edu]]
 #SBATCH --mem=4G
 
-module load python
-cd [[path to working directory]]
-python [[script name]]
+module load python/anaconda3.6
+cd [[//projects/b1048/gillanes/Mergers/Codes/Mergers]]
+python [[GenerateSampleBeerData.py]]
