bedbdd4477f20328f52c96c276851af6709f40dd
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-02-19 17:25:09 -0600

    new sh file

---
+++ AggregateMovement.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="Mergers Generate Beer Data"
+#SBATCH -A b1048
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 4:00:00
+#SBATCH -p buyin
+#SBATCH --mail-user=yintianzhan2021@u.northwestern.edu
+#SBATCH --mem=20G
+
+module load python/anaconda3.6
+cd /projects/b1048/gillanes/Mergers/Codes/Mergers
+python AggregateMovement.py 5001 2006 2009
