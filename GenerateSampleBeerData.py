f6127783ad501dddc31d0cc7ae9473aa511104a1
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-02-19 16:28:58 -0600

    combine products and movement

--- GenerateSampleBeerData.py
+++ GenerateSampleBeerData.py
@@ -3,11 +3,10 @@ from ProductModuleMap import productModuleMap
 from tqdm import tqdm
 
 # load products data and find the product entries, ex. FindProduct("BEER")
-def LoadWantedProduct(product):
+def LoadWantedProduct(product_group_code):
     products_path = "../../Data/nielsen_extracts/RMS/Master_Files/Latest/products.tsv"
-    products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0)
-    wantedProducts = products[(products['product_group_descr'].notnull()) & (products['product_group_descr'].str.contains(product))]
-    print(wantedProducts['brand_descr'].unique().shape)
+    products = pd.read_csv(products_path, delimiter = "\t", encoding = "cp1252", header = 0, index_col = "upc")
+    wantedProducts = products[products['product_group_code'] == product_group_code]
     print("Loaded "+product+" products")
     return wantedProducts
 
@@ -42,17 +41,15 @@ def LoadChunkedYearModuleMovementTable(year, group, module):
     return movementTable
 
 
-
-beerProducts = LoadWantedProduct("BEER")
 # record beer upcs
-beer_UPCs = {}
-for index, row in beerProducts.iterrows():
-    upc = row['upc']
-    beer_UPCs[upc] = 0
-    # for purpose of running quickly
-    if len(beer_UPCs) >= 10:
-        break
-print("Finished recording beer UPCs")
+# beer_UPCs = {}
+# for index, row in beerProducts.iterrows():
+#     upc = row['upc']
+#     beer_UPCs[upc] = 0
+#     # for purpose of running quickly
+#     if len(beer_UPCs) >= 10:
+#         break
+# print("Finished recording beer UPCs")
 
 
 
@@ -60,7 +57,9 @@ print("Finished recording beer UPCs")
 years = ['2006','2007','2008','2009']
 groups = [5001]
 modules = [5000,5001,5005,5010,5015,5020]
-aggregation_function = {'week_end': 'first', 'units': 'sum', 'prmult':'mean', 'price':'mean', 'feature': 'first','display':'first','store_code_uc':'first'}
+aggregation_function = {'week_end': 'first', 'units': 'sum', 'prmult':'mean', 'price':'mean', 'feature': 'first','display':'first','store_code_uc':'first','sales':'sum'}
+products = LoadWantedProduct("BEER")
+productMap = products.to_dict()
 for year in years:
     area_month_upc_Year = []
     storeTable = LoadStoreTable(year)
@@ -75,8 +74,13 @@ for year in years:
                 data_chunk['month'] = data_chunk['week_end']/100
                 data_chunk['month'] = data_chunk['month'].astype(int)
                 data_chunk['dma_code'] = data_chunk['store_code_uc'].map(dmaMap)
+                data_chunk['sales'] = data_chunk['price'] * data_chunk['units'] / data_chunk['prmult']
                 # data_chunk['fips_state_code'] = data_chunk.apply(lambda x: storeTable.loc[x['store_code_uc']].fips_state_code, axis = 1)
                 # data_chunk['fips_county_code'] = data_chunk.apply(lambda x: storeTable.loc[x['store_code_uc']].fips_county_code, axis = 1)
+                # data_chunk['size1_amount'] = data_chunk['upc'].map(productMap['size1_amount'])
+                # data_chunk['multi'] = data_chunk['upc'].map(productMap['multi'])
+                # data_chunk['brand_code_uc'] = data_chunk['upc'].map(productMap['brand_code_uc'])
+                # data_chunk['brand_descr'] = data_chunk['upc'].map(productMap['brand_descr'])
                 area_month_upc = data_chunk.groupby(['month', 'upc','dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = data_chunk.columns)
                 area_month_upc_list.append(area_month_upc)
             area_month_upc = pd.concat(area_month_upc_list)
@@ -86,6 +90,11 @@ for year in years:
 
     area_month_upc = pd.concat(area_month_upc_Year)
     area_month_upc = area_month_upc.groupby(['month', 'upc','dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = area_month_upc.columns)
+    area_month_upc['brand_code_uc'] = area_month_upc['upc'].map(productMap['brand_code_uc'])
+    area_month_upc['brand_descr'] = area_month_upc['upc'].map(productMap['brand_descr'])
+    area_month_upc['multi'] = area_month_upc['upc'].map(productMap['multi'])
+    area_month_upc['size1_amount'] = area_month_upc['upc'].map(productMap['size1_amount'])
+    area_month_upc['volume'] = area_month_upc['units'] * area_month_upc['size1_amount'] * area_month_upc['multi']
     area_month_upc.drop(['week_end','store_code_uc'], axis=1, inplace=True)
     area_month_upc.to_csv("../../GeneratedData/BEER_dma_month_upc_"+year+".tsv", sep = '\t', encoding = 'utf-8')
     print("Saved dma_month_upc data for year "+year)
