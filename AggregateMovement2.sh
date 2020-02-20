541103672e2f4a98e87feafe2835996687139a51
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-02-19 23:55:25 -0600

    try different processor

---
+++ AggregateMovement2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="aggregate movement files"
+#SBATCH -A p30927
+#SBATCH -p short
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -c 4
+#SBATCH -t 1:00:00
+#SBATCH --mail-user=gastonillanes@gmail.com
+#SBATCH --array=1
+#SBATCH --mem=0G
+#SBATCH --nodes=1
+
+module load python/anaconda3.6
+cd /projects/b1048/gillanes/Mergers/Codes/Mergers
+python AggregateMovement.py 0503 2006 2009
