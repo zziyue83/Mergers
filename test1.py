f0226056c8d4a68b3a4a7f69925e7800755116aa
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-02-24 23:55:32 -0600

    test generated data

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from tqdm import tqdm
-products = pd.read_csv("../../GeneratedData/"+"CANDY"+"_dma_month_upc_"+"2009"+".tsv", header = 0, delimiter = "\t", index_col = "upc", chunksize = 100000)
+products = pd.read_csv("../../GeneratedData/"+"CANDY"+"_dma_month_upc_"+"2009"+".tsv", header = 0, delimiter = "\t", index_col = "upc", chunksize = 10000001)
 brandsVolume = pd.DataFrame()
 i = 0
 for data_chunk in tqdm(products):
-    if i == 2:
+    if i == 1:
         break
     else:
-        a = data_chunk
+        print(data_chunk.iloc[-1])
         i = i + 1
 
-print(a[a['volume']=='volume'])
+# print(a[a['volume']=='volume'])
