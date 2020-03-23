8101e4d447fe4addb114441e89fee7c7718694e3
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-23 15:06:19 -0500

    1.2

--- MatchDMA.py
+++ MatchDMA.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from major_us_city_dma_codes import major_cities
 
-movement = pd.read_csv("../../GeneratedData/"+product+"_dma_month_upc_"+year+".tsv", delimiter = '\t' , index_col = "upc" , chunksize = 1000000)
+movement = pd.read_csv("../../GeneratedData/BEER_dma_month_upc_2006.tsv", delimiter = '\t' , index_col = "upc" , chunksize = 1000000)
 dmaDf = pd.DataFrame(major_cities)
 citydma = dmaDf['dma_code'].tolist()
 movementdma = movement['dma_code'].unique()
