b420fde06a16c08c481641fa2c4a66a2d73279e6
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-27 11:29:02 -0500

    2.0

--- CalDMAMktSize.py
+++ CalDMAMktSize.py
@@ -8,27 +8,28 @@ def GenerateYearList(start, end):
     return list(range(s, e+1))
 
 def CalDMAMktSize(product, years, size_multiplier):
+    area_quarter_mkt_list = []
     years = list(map(str,years))
-    brandsCumuYear = []
     aggregation_function = {'units': 'sum', 'prmult':'mean', 'price':'mean', 'feature': 'first','display':'first','sales':'sum', 'volume':'sum','month':'first','brand_descr':'first','brand_code_uc':'first','multi':'first','size1_amount':'first'}
-    mkt_size_agg_function = {'volume': 'max', 'quarter':'first'}
-    mkt_size
     for year in years:
         firstFile = True
-        savePath = "../../GeneratedData/"+product+"_dma_quarter_mkt_size_"+year+".tsv"
         movement = pd.read_csv("../../GeneratedData/"+product+"_dma_quarter_upc_"+year+".tsv", delimiter = '\t', chunksize = 100000)
         for data_chunk in tqdm(movement):
             area_quarter = data_chunk.groupby(['quarter','dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = data_chunk.columns)
             area_quarter = area_quarter_upc[['quarter','dma_code','volume']]
             area_quarter['volume'] = area_quarter * size_multiplier
-            area_quarter = data_chunk.groupby(['dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = area_quarter.columns)
-            area_month_upc.drop(['quarter'], axis=1, inplace=True)
-            if firstFile:
-                area_quarter.to_csv(savePath, sep = '\t', encoding = 'utf-8')
-                firstFile = False
-                print(area_quarter.iloc[0])
-            else:
-                area_quarter.to_csv(savePath, sep = '\t', encoding = 'utf-8', mode = 'a', header = False)
+            area_quarter_mkt_list.append(area_quarter)
+
+    savePath = "../../GeneratedData/"+product+"__mkt_size_"+year+".tsv"
+    mkt_size_agg_function = {'volume': 'max', 'quarter':'first'}
+    area_quarter_agg = pd.cancat(area_quarter_mkt_list)
+    area_quarter_agg = area_quarter_agg.groupby(['dma_code'], as_index = False).aggregate(mkt_size_agg_function).reindex(columns = area_quarter_agg.columns)
+    area_quarter_agg.drop(['quarter'], axis=1, inplace=True)
+    area_quarter_agg.to_csv(savePath, sep = '\t', encoding = 'utf-8')
+            #     firstFile = False
+            #     print(area_quarter.iloc[0])
+            # else:
+            #     area_quarter.to_csv(savePath, sep = '\t', encoding = 'utf-8', mode = 'a', header = False)
 
 
 
