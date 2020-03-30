12ea14fdb0a47bf5ed6634ec6b618fd726ccf26b
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-30 18:09:48 -0500

    generalized calculate market size to month and quarter

--- CalDMAMktSize.py
+++ CalDMAMktSize.py
@@ -7,20 +7,20 @@ def GenerateYearList(start, end):
     e = int(end)
     return list(range(s, e+1))
 
-def CalDMAMktSize(product, years, size_multiplier):
+def CalDMAMktSize(product, years, size_multiplier, frequency):
     area_quarter_mkt_list = []
     years = list(map(str,years))
     aggregation_function = {'units': 'sum', 'prmult':'mean', 'price':'mean', 'feature': 'first','display':'first','sales':'sum', 'volume':'sum','month':'first','brand_descr':'first','brand_code_uc':'first','multi':'first','size1_amount':'first'}
     for year in years:
         firstFile = True
-        movement = pd.read_csv("../../GeneratedData/"+product+"_dma_quarter_upc_"+year+".tsv", delimiter = '\t', chunksize = 100000)
+        movement = pd.read_csv("../../GeneratedData/"+product+"_dma_"+frequency+"_upc_"+year+".tsv", delimiter = '\t', chunksize = 100000)
         for data_chunk in tqdm(movement):
-            area_quarter = data_chunk.groupby(['quarter','dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = data_chunk.columns)
-            area_quarter = area_quarter[['quarter','dma_code','volume']]
+            area_quarter = data_chunk.groupby([frequency,'dma_code'], as_index = False).aggregate(aggregation_function).reindex(columns = data_chunk.columns)
+            area_quarter = area_quarter[[frequency,'dma_code','volume']]
             area_quarter['volume'] = area_quarter['volume'] * size_multiplier
             area_quarter_mkt_list.append(area_quarter)
 
-    savePath = "../../GeneratedData/"+product+"_dma_mkt_size.tsv"
+    savePath = "../../GeneratedData/"+product+"_dma_"+frequency+"_mkt_size.tsv"
     mkt_size_agg_function = {'volume': 'max', 'quarter':'first'}
     area_quarter_agg = pd.concat(area_quarter_mkt_list)
     area_quarter_agg = area_quarter_agg.groupby(['dma_code'], as_index = False).aggregate(mkt_size_agg_function).reindex(columns = area_quarter_agg.columns)
@@ -36,15 +36,16 @@ def CalDMAMktSize(product, years, size_multiplier):
 
 
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print("Not enough arguments")
     sys.exit()
 
 start = sys.argv[1]
 end = sys.argv[2]
 product = sys.argv[3]
+frequency = sys.argv[4]
 years = GenerateYearList(start, end)
 print(product)
 print(years)
-CalDMAMktSize(product, years,1.5)
+CalDMAMktSize(product, years, 1.5, frequency)
 # >>>>>>> fc0f14f7f4cc63df8fc9b9cfc1a730ed3a969f91'
