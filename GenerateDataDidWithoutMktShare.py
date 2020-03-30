c306ee7555e72a83af2e71215f42f86aa337b509
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-30 17:07:16 -0500

    3.2

--- GenerateDataDidWithoutMktShare.py
+++ GenerateDataDidWithoutMktShare.py
@@ -36,6 +36,7 @@ def MakeTimeDummy(quarters, mergingq, startq):
 
 def AddOwnerandTimeVariables(product, years, mergers, mergingq, startq):
     owners = pd.read_csv("Top 100 "+product+".csv", delimiter = ',')
+    print(owners.columns)
     all_owners = owners['owner inital'].unique()
     ownerDummyDf = MakeOwnerDummy(mergers, all_owners)
     print(ownerDummyDf)
