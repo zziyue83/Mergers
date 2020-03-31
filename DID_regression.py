15f1332515e1fcfa884bdc5c91d94beb4f8a141b
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-31 13:12:31 -0500

    combine dma and upc as index

--- DID_regression.py
+++ DID_regression.py
@@ -8,8 +8,9 @@ def DID_regression(product, frequency, share):
     if share == 'NoMktShare':
         data = pd.read_csv("../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv", delimiter = '\t')
         data['post_merger*merging'] = data['post_merger']*data['merging']
-        data['lprice'] = np.log(data['price'])
-        data = data.set_index(['dma_code','upc'])
+        data['dma_upc'] = data['dma_code'].astype(str) + "_" + data['upc'].astype(str)
+        data['lprice_'+product] = np.log(data['price'])
+        data = data.set_index(['dma_upc'])
         exog_vars = ['post_merger*merging', 'post_merger', frequency+'s_since_start']
         exog = sm.add_constant(data[exog_vars])
         mod = PanelOLS(data['lprice'], exog, entity_effects = True)
