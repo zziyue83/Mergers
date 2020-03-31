b39c5400ace8c2a17eeb948541dab5b0b1df3beb
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-31 01:32:32 -0500

    2.4

--- DID_without_share.py
+++ DID_without_share.py
@@ -9,7 +9,7 @@ def DID_regression(product, frequency, share):
         data = pd.read_csv("../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv", delimiter = '\t')
         data['post_merger*merging'] = data['post_merger']*data['merging']
         data['lprice'] = np.log(data['price'])
-        data.set_index(['dma_code','upc'])
+        data = data.set_index(['dma_code','upc'])
         exog_vars = ['post_merger*merging', 'post_merger', frequency+'s_since_start']
         exog = sm.add_constant(data[exog_vars])
         mod = PanelOLS(data['lprice'], exog, entity_effects = True)
