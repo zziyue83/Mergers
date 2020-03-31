60b643d05a80ab88a48be61bb7c3a54305c6efe8
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-03-31 01:27:09 -0500

    2.0

--- DID_without_share.py
+++ DID_without_share.py
@@ -4,15 +4,16 @@ import statsmodels.api as sm
 import numpy as np
 
 def DID_regression(product, frequency, share):
-    data = pd.read_csv("../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv", delimiter = '\t')
-    data['post_merger*merging'] = data['post_merger']*data['merging']
-    data['lprice'] = np.log(data['price'])
-    data.set_index(['dma_code','upc'])
-    exog_vars = ['post_merger*merging', 'post_merger']
-    exog = sm.add_constant(data[exog_vars])
-    mod = PooledOLS(data['lprice'], exog, entity_effects = True)
-    fe_res = mod.fit()
-    print(fe_res)
+    if share == 'NoMktShare':
+        data = pd.read_csv("../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv", delimiter = '\t')
+        data['post_merger*merging'] = data['post_merger']*data['merging']
+        data['lprice'] = np.log(data['price'])
+        data.set_index(['dma_code','upc'])
+        exog_vars = ['post_merger*merging', 'post_merger']
+        exog = sm.add_constant(data[exog_vars])
+        mod = PooledOLS(data['lprice'], exog, entity_effects = True)
+        fe_res = mod.fit()
+        print(fe_res)
 
 if len(sys.argv) < 3:
     print("Not enough arguments")
