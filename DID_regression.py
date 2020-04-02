cc26d2659700750046a0561125d5b0b4b0a42b77
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-02 17:16:57 -0500

    make did regression with mktshare

--- DID_regression.py
+++ DID_regression.py
@@ -30,28 +30,32 @@ def DID_regression(product, frequency, share):
         f.close()
     elif share == 'MktShare':
         data = pd.read_csv("../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv", delimiter = '\t')
-        volumes = data.groupby(['dma','post_merger','owner'])
-        data['DHHI'] = data['HHIAfter'] - data['HHIBefore']
-        data['DHHI*post_merger'] = data['DHHI']*data['merging']
-        data['dma_upc'] = data['dma_code'].astype(str) + "_" + data['upc'].astype(str)
-        data['lprice_'+product] = np.log(data['price'])
-        data['trend'] = data[frequency+'s_since_start']
-        data = data.set_index(['dma_upc',frequency+'s_since_start'])
-        exog_vars = ['DHHI*post_merger', 'post_merger', 'trend']
-        exog = sm.add_constant(data[exog_vars])
-        mod = PanelOLS(data['lprice_' + product], exog, entity_effects = True)
-        fe_res = mod.fit()
-        print(fe_res)
-
-        beginningtex = """\\documentclass{report}
-                          \\usepackage{booktabs}
-                          \\begin{document}"""
-        endtex = "\end{document}"
-        f = open(product + '_DID_MktShare.tex', 'w')
-        f.write(beginningtex)
-        f.write(fe_res.summary.as_latex())
-        f.write(endtex)
-        f.close()
+        dma_month_volume = pd.read_csv("../../GeneratedData/"+product+"_dma_every_"+frequency+"_mkt_volume.tsv", delimiter = '\t')
+        dma_month_volume = dma_month_volume.merge(data[[frequency,'post_merger']], how = 'inner', left_on = frequency, right_on = frequency)
+        dma_pre_post_merger_volume = dma_monthvolume.groupby(['dma','post_merger'], as_index = False).aggregate({'volume': 'sum'}).reindex(columns = dma_month_volume.columns)
+        print(dma_pre_post_merger_volume)
+        # volumes = data.groupby(['dma','post_merger','owner'])
+        # data['DHHI'] = data['HHIAfter'] - data['HHIBefore']
+        # data['DHHI*post_merger'] = data['DHHI']*data['merging']
+        # data['dma_upc'] = data['dma_code'].astype(str) + "_" + data['upc'].astype(str)
+        # data['lprice_'+product] = np.log(data['price'])
+        # data['trend'] = data[frequency+'s_since_start']
+        # data = data.set_index(['dma_upc',frequency+'s_since_start'])
+        # exog_vars = ['DHHI*post_merger', 'post_merger', 'trend']
+        # exog = sm.add_constant(data[exog_vars])
+        # mod = PanelOLS(data['lprice_' + product], exog, entity_effects = True)
+        # fe_res = mod.fit()
+        # print(fe_res)
+        #
+        # beginningtex = """\\documentclass{report}
+        #                   \\usepackage{booktabs}
+        #                   \\begin{document}"""
+        # endtex = "\end{document}"
+        # f = open(product + '_DID_MktShare.tex', 'w')
+        # f.write(beginningtex)
+        # f.write(fe_res.summary.as_latex())
+        # f.write(endtex)
+        # f.close()
 
 if len(sys.argv) < 3:
     print("Not enough arguments")
