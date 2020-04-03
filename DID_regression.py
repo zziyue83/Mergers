3f4ab909d0f298e333d8077612502aa7a7615c07
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-03 13:20:52 -0500

    1.3

--- DID_regression.py
+++ DID_regression.py
@@ -100,7 +100,7 @@ def DID_regression(product, frequency, share, mergingt, mergers):
         firmDMA['share_square_post_merger'] = firmDMA['share_square'] * firmDMA['post_merger'] * (1 - firmDMA['merging'])
         firmDMA['share_square_pre_merger'] = firmDMA['share_square'] * (1 - firmDMA['post_merger'])
         merger = firmDMA[(firmDMA['post_merger'] == 1) & (firmDMA['merging'] == 1)]
-        merger = merger.groupby(['dma']).agg({'share':'sum','dma_size':'first','volume':'sum','owner':'first','merging':'first','post_merger': 'first'}, as_index = False).reindex(columns = firmDMA.columns)
+        merger = merger.groupby(['dma_code']).agg({'share':'sum','dma_size':'first','volume':'sum','owner':'first','merging':'first','post_merger': 'first'}, as_index = False).reindex(columns = firmDMA.columns)
         merger['share_square'] = merger['share'] * merger['share']
         merger['share_square_post_merger'] = merger['share_square'] * merger['post_merger']
         merger['share_square_pre_merger'] = 0
