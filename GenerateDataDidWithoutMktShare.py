0de4fef3865238e84af18a99c6218f1356ad5805
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-02 23:58:48 -0500

    3.2

--- GenerateDataDidWithoutMktShare.py
+++ GenerateDataDidWithoutMktShare.py
@@ -93,7 +93,7 @@ def AddOwnerandTimeVariables(product, years, mergers, mergingt, startt, frequenc
 
     savePath = "../../GeneratedData/"+product+"_DID_without_share_"+frequency+".tsv"
     DID_agg = pd.concat(DID_list)
-    DID_agg = DID_agg[['upc','price','dma_code','brand_code_uc','brand_descr','post_merger',frequency+'s_since_start',frequency,'owner','merging']]
+    DID_agg = DID_agg[['upc','price','dma_code','brand_code_uc','brand_descr','post_merger',frequency+'s_since_start',frequency,'owner','merging','volume']]
     # DID_agg = DID_agg.rename(columns = {'brand_descr_x': 'brand_descr'})
     DID_agg.to_csv(savePath, sep = '\t')
 
