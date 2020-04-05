8137d135b97878c9ae1784ad16f8ff8dc6e18513
Aisling Chen <zcw7438@quser23.quest.it.northwestern.edu>
2020-04-04 23:48:37 -0500

    ‘test1_’

--- AggregateToMonthOrQuarter_A.sh
+++ AggregateToMonthOrQuarter_A.sh
@@ -12,4 +12,4 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python AggregateToMonthOrQuarter_A.py GUM quarter 200805
+python AggregateToMonthOrQuarter_A.py BEER month 200807
