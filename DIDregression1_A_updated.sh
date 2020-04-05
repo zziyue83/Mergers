d8e263c889f293b2192144bb1aa7caed11a0e5c6
Yintian Zhan <yza4307@quser22.quest.it.northwestern.edu>
2020-04-05 00:26:57 -0500

    new tex files

---
+++ DIDregression1_A_updated.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="DID1"
+#SBATCH -A b1048
+#SBATCH -p buyin
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -c 4
+#SBATCH -t 1:00:00
+#SBATCH --mail-user=ziyuechen2022@u.northwestern.edu
+#SBATCH --array=1
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+
+module load python/anaconda3.6
+cd /projects/b1048/gillanes/Mergers/Codes/Mergers
+python DIDregression1_A.py BEER quarter
