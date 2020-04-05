d67a25d1812e46f992f1e2881a978c189f1f4557
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-05 12:04:32 -0500

    1.5

--- DID_regression.sh
+++ DID_regression.sh
@@ -11,4 +11,4 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python DID_regression.py CANDY quarter MktShare 200805
+python DID_regression.py CANDY quarter NoMktShare 200805
