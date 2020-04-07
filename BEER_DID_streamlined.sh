b0652c76bda50f530f50cc7bde4f16a7128c4b96
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-06 22:16:06 -0500

    added candy streamlined

--- BEER_DID_streamlined.sh
+++ BEER_DID_streamlined.sh
@@ -11,7 +11,7 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python GenerateDataDidWithoutMktShare.py 2006 2009 BEER quarter 2008Q3 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 BEER quarter 2008Q3 2006Q1
 python DID_regression.py BEER quarter NoMktShare 2008Q3
 python DID_regression.py BEER quarter MktShare 2008Q3
 python GenerateDataDidWithoutMktShare.py 2006 2009 BEER month 200807 200601
---
+++ CANDY_DID_streamlined.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name="DID regression in one file"
+#SBATCH -A b1048
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 4:00:00
+#SBATCH -p buyin
+#SBATCH --mail-user=yintianzhan2021@u.northwestern.edu
+#SBATCH --mem=20G
+
+module load python/anaconda3.6
+cd /projects/b1048/gillanes/Mergers/Codes/Mergers
+python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 2008Q2 2006Q1
+python GenerateDataDidWithoutMktShare.py 2006 2009 GUM quarter 2008Q2 2006Q1
+python DID_regression.py CANDY quarter NoMktShare 2008Q2
+python DID_regression.py CANDY quarter MktShare 2008Q2
+python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY month 200805 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 GUM month 200805 200601
+python DID_regression.py CANDY month NoMktShare 200805
+python DID_regression.py CANDY month MktShare 200805
+pdflatex CANDY_DID_MktShare_month.tex
+pdflatex CANDY_DID_NoMktShare_month.tex
+pdflatex CANDY_DID_MktShare_quarter.tex
+pdflatex CANDY_DID_NoMktShare_quarter.tex
