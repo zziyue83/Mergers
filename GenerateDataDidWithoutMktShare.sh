65664f084ed3905e9607b8b2fee3f5817d5649bc
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-06 20:03:35 -0500

    corrected merging quarter specificaton

--- GenerateDataDidWithoutMktShare.sh
+++ GenerateDataDidWithoutMktShare.sh
@@ -11,4 +11,4 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 200805 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 2008Q2 200601
