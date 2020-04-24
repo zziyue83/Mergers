d4d1bcfbe745f06ca635da6cac1d1119cb8cf892
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-24 13:51:24 -0500

    1.9

--- GrabNutritionInfo.py
+++ GrabNutritionInfo.py
@@ -3,8 +3,9 @@ from urllib.request import urlopen
 import pandas as pd
 
 data = pd.read_csv("../../GeneratedData/"+'BEER'+"_DID_without_share_"+'month'+".tsv", delimiter = '\t')
+data['upc_str'] = data['upc'].astype(str)
 for index, row in data.iterrows():
-    upc = row['upc'].astype(str)
+    upc = row['upc_str']
     url = "https://api.edamam.com/api/food-database/parser?upc="+upc+"&app_id=4237ba9d&app_key=56559448d34967c26665a71a519650e6"
     response = urlopen(url)
     data = json.loads(response.read())
