a309e378bbbd0c6c0c016aa6789acee3a316d18c
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-04-24 14:04:15 -0500

    2.0

--- GrabNutritionInfo.py
+++ GrabNutritionInfo.py
@@ -2,15 +2,24 @@ import json
 from urllib.request import urlopen
 import pandas as pd
 
+
+# url = "https://api.edamam.com/api/food-database/parser?upc=034100575052&app_id=4237ba9d&app_key=56559448d34967c26665a71a519650e6"
+# response = urlopen(url)
+# data = json.loads(response.read())
+# print(data)
+
 data = pd.read_csv("../../GeneratedData/"+'BEER'+"_DID_without_share_"+'month'+".tsv", delimiter = '\t')
 data['upc_str'] = data['upc'].astype(str)
 for index, row in data.iterrows():
     upc = row['upc_str']
     print(upc)
-    url = "https://api.edamam.com/api/food-database/parser?upc="+"948700007"+"&app_id=4237ba9d&app_key=56559448d34967c26665a71a519650e6"
-    response = urlopen(url)
-    data = json.loads(response.read())
-    print(data)
+    try:
+        url = "https://api.edamam.com/api/food-database/parser?upc="+"948700007"+"&app_id=4237ba9d&app_key=56559448d34967c26665a71a519650e6"
+        response = urlopen(url)
+        data = json.loads(response.read())
+        print(data)
+    except:
+        print('could not find nutrition data for upc '+upc)
 
 
 # import json
