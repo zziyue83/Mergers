409d23a06c61750a5e03bfaa926dc383644497ef
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 00:25:25 -0500

    1.8

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -4,6 +4,7 @@ import sys
 def GenerateDEData(product, frequency):
     data = pd.read_csv("../../GeneratedData/" + product + "_pre_model_" + frequency + "_with_distance.tsv", delimiter = '\t')
     print(data)
+    print(data.columns)
 
 frequency = sys.argv[1]
 product = sys.argv[2]
