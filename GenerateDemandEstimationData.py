c65cd8f9ff55f8fad47fb2522f6386855d79bd7f
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 17:38:14 -0500

    haha

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -30,7 +30,7 @@ def GenerateDEData(product, frequency, inputs):
     print(demand_estimation_data.head())
     rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
     for i in range(len(inputs)):
-        rename_dic[inputs[i]] = 'demand_instruments'+str(i)
+        rename_dic[inputs[i]] = 'demand_instruments'+str(i+1)
     demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
     print(demand_estimation_data.head())
     # pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
