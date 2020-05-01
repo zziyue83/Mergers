7c3bfbee49f45547d1db3f15391f06bbb63bb980
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 01:12:07 -0500

    2.1

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -6,8 +6,13 @@ def GenerateDEData(product, frequency):
     print(data)
     print(data.columns)
     print(data['market_share'])
-    demand_estimation_data = data[['dma_code','log_adjusted_price','Firm','brand_descr',frequency+'_since_start','upc']]
+    demand_estimation_data = data[['dma_code','log_adjusted_price','Firm','brand_descr',frequency+'_since_start','upc','market_share']]
     print(demand_estimation_data)
+    rename_dic = {'dma_code':'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
+    demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
+    print(demand_estimation_data.head())
+
+
 frequency = sys.argv[1]
 product = sys.argv[2]
 GenerateDEData(product, frequency)
