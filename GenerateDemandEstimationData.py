0326a5c26b0cbf49560862b1ca4c38611209ba6b
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 01:28:20 -0500

    3,2

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -5,7 +5,7 @@ import pyblp
 def GenerateDEData(product, frequency):
     data = pd.read_csv("../../GeneratedData/" + product + "_pre_model_" + frequency + "_with_distance.tsv", delimiter = '\t')
     data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
-    demand_estimation_data = data[['dma_code_'+frequency,'log_adjusted_price','Firm','brand_descr',frequency+'_since_start','upc','market_share']]
+    demand_estimation_data = data[['dma_code_'+frequency,'log_adjusted_price',frequency+'_since_start','upc','market_share']]
     print(demand_estimation_data.head())
     rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
     demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
