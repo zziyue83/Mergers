8582f4aeaf49519b89e000b3eabea6dc23a639c0
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 17:20:13 -0500

    6.7

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -4,30 +4,35 @@ import pyblp
 
 def GenerateDEData(product, frequency, inputs):
     data = pd.read_csv("../../GeneratedData/" + product + "_pre_model_" + frequency + "_with_distance.tsv", delimiter = '\t')
-    print(data['y-m-d'])
-    for input in inputs:
-        input_prices = ReadInstrument(input)
-        data['y-m'] = pd.to_datetime(data['y-m-d']).dt.to_period('M')
-        data = data.merge(input_prices, how = 'inner', left_on = 'y-m', right_on = 't')
-        print(data.head())
-    data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
-    variables = ['dma_code_'+frequency,'log_adjusted_price','upc','market_share']
-    for input in inputs:
-        variables.append(input)
-    print(variables)
-    demand_estimation_data = data[variables]
-    print(demand_estimation_data.head())
-    rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
-    for i in range(len(inputs)):
-        rename_dic[inputs[i]] = 'demand_instruments'+str(i)
-    demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
-    print(demand_estimation_data.head())
-    pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
-    logit_formulation = pyblp.Formulation('prices')
-    problem = pyblp.Problem(logit_formulation, demand_estimation_data)
-    print(problem)
-    logit_results = problem.solve()
-    print(logit_results)
+    # print(data['y-m-d'])
+    # for input in inputs:
+    #     input_prices = ReadInstrument(input)
+    #     data['y-m'] = pd.to_datetime(data['y-m-d']).dt.to_period('M')
+    #     data = data.merge(input_prices, how = 'inner', left_on = 'y-m', right_on = 't')
+    #     print(data.head())
+    # data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
+    x = data['distance']
+    x['constant'] = 1
+    print(x)
+    y = np.linalg.inv(x)
+
+    # variables = ['dma_code_'+frequency,'log_adjusted_price','upc','market_share']
+    # for input in inputs:
+    #     variables.append(input)
+    # print(variables)
+    # demand_estimation_data = data[variables]
+    # print(demand_estimation_data.head())
+    # rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
+    # for i in range(len(inputs)):
+    #     rename_dic[inputs[i]] = 'demand_instruments'+str(i)
+    # demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
+    # print(demand_estimation_data.head())
+    # pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
+    # logit_formulation = pyblp.Formulation('prices')
+    # problem = pyblp.Problem(logit_formulation, demand_estimation_data)
+    # print(problem)
+    # logit_results = problem.solve()
+    # print(logit_results)
 
 def ReadInstrument(input, skiprows = 0):
     instrument = pd.read_csv(input+'.csv', skiprows = skiprows, delimiter = ',')
@@ -41,7 +46,7 @@ def ReadInstrument(input, skiprows = 0):
 
 frequency = sys.argv[1]
 product = sys.argv[2]
-input = 'barley'
-instrument = ReadInstrument(input)
-print(instrument['t'])
+# input = 'barley'
+# instrument = ReadInstrument(input)
+# print(instrument['t'])
 GenerateDEData(product, frequency, inputs = ['wheat'])
