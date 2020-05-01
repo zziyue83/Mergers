f16cb7debe9d6add10947fab20b0bed0fa72423e
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 16:23:19 -0500

    5.2

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -5,7 +5,7 @@ import pyblp
 def GenerateDEData(product, frequency, input):
     data = pd.read_csv("../../GeneratedData/" + product + "_pre_model_" + frequency + "_with_distance.tsv", delimiter = '\t')
     print(data['y-m-d'])
-    input_prices = ReadInstrument(input, skiprows = 15)
+    input_prices = ReadInstrument(input)
     data['y-m'] = pd.to_datetime(data['y-m-d']).dt.to_period('M')
     data = data.merge(input_prices, how = 'inner', left_on = 'y-m', right_on = 't')
     data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
@@ -14,7 +14,7 @@ def GenerateDEData(product, frequency, input):
     rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares',input:'demand_instruments1'}
     demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
     print(demand_estimation_data.head())
-    # pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
+    pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
     logit_formulation = pyblp.Formulation('prices')
     problem = pyblp.Problem(logit_formulation, demand_estimation_data)
     print(problem)
@@ -32,7 +32,7 @@ def ReadInstrument(input, skiprows = 0):
 
 frequency = sys.argv[1]
 product = sys.argv[2]
-input = 'wheat'
-instrument = ReadInstrument(input,15)
+input = 'barley'
+instrument = ReadInstrument(input)
 print(instrument)
 GenerateDEData(product, frequency, input = 'wheat')
