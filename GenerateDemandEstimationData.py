5afcc1c16eb8054b144ba49bd16c4b53b589531a
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-01 01:45:38 -0500

    3.5

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -10,7 +10,7 @@ def GenerateDEData(product, frequency):
     rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
     demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
     print(demand_estimation_data.head())
-    logit_formulation = pyblp.Formulation('prices', absorb='C(product_ids)')
+    logit_formulation = pyblp.Formulation('prices')
     problem = pyblp.Problem(logit_formulation, demand_estimation_data)
     print(problem)
     logit_results = problem.solve()
