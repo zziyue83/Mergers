170f8373748b553c8797391df94e33301a287382
Yintian Zhan <yza4307@quser23.quest.it.northwestern.edu>
2020-05-02 12:17:02 -0500

    chmod

--- AggregateToMonthOrQuarter_A.sh
+++ AggregateToMonthOrQuarter_A.sh
@@ -12,4 +12,4 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python AggregateToMonthOrQuarter_A.py GUM month
+python AggregateToMonthOrQuarter_A.py GUM quarter
