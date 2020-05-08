7397ab33c3a1b24ce5e27541dbe317bba83d1ed1
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-07 22:56:18 -0500

    re-generate candy gum panel data

--- GenerateDataDidWithoutMktShare.sh
+++ GenerateDataDidWithoutMktShare.sh
@@ -11,5 +11,9 @@
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
-python GenerateDataDidWithoutMktShare.py 2006 2009 BEER month 200807 200601
-python GenerateDataDidWithoutMktShare.py 2006 2009 BEER quarter 2008Q3 200601
+# python GenerateDataDidWithoutMktShare.py 2006 2009 BEER month 200807 200601
+# python GenerateDataDidWithoutMktShare.py 2006 2009 BEER quarter 2008Q3 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY month 200805 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 2008Q2 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 GUM month 200805 200601
+python GenerateDataDidWithoutMktShare.py 2006 2009 GUM quarter 2008Q2 200601
