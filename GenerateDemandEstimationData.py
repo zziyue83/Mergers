4a15783ac9995737ce3bdeb9f93e83c13915fddf
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-08 16:19:59 -0500

    add time fixed effect again

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -37,17 +37,17 @@ def GenerateDEData(product, frequency, inputs, characteristics, start, end):
     # print(x)
     # u = np.linalg.inv(y)
     # print(u)
-    variables = ['dma_code_'+frequency,'log_adjusted_price','upc','market_share','distance','y-m'] + characteristics + inputs
+    variables = ['dma_code_'+frequency,'adjusted_price','upc','market_share','distance','y-m'] + characteristics + inputs
     print(variables)
     demand_estimation_data = data[variables]
     print(demand_estimation_data.head())
-    rename_dic = {'dma_code_'+frequency:'market_ids','log_adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares'}
+    rename_dic = {'dma_code_'+frequency:'market_ids','adjusted_price':'prices','Firm':'firm_ids','brand_descr':'brand_ids',frequency+'_since_start':frequency,'upc':'product_ids','distance':'demand_instruments0','market_share':'shares','y-m':'time'}
     for i in range(len(inputs)):
         rename_dic[inputs[i]] = 'demand_instruments'+str(i+1)
     demand_estimation_data = demand_estimation_data.rename(columns = rename_dic)
     print(demand_estimation_data.head())
     # pyblp.options.collinear_atol = pyblp.options.collinear_rtol = 0
-    logit_formulation = pyblp.Formulation('prices', absorb='C(product_ids) + C(y-m)')
+    logit_formulation = pyblp.Formulation('prices', absorb='C(product_ids) + C(time)')
     problem = pyblp.Problem(logit_formulation, demand_estimation_data)
     print(problem)
     logit_results = problem.solve()
