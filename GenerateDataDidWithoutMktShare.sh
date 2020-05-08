48b3b0e073842d1dc301323bdb8228e56692194c
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-07 23:24:39 -0500

    again

--- GenerateDataDidWithoutMktShare.sh
+++ GenerateDataDidWithoutMktShare.sh
@@ -7,13 +7,13 @@
 #SBATCH -t 4:00:00
 #SBATCH -p buyin
 #SBATCH --mail-user=yintianzhan2021@u.northwestern.edu
-#SBATCH --mem=20G
+#SBATCH --mem=0G
 
 module load python/anaconda3.6
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers
 # python GenerateDataDidWithoutMktShare.py 2006 2009 BEER month 200807 200601
 # python GenerateDataDidWithoutMktShare.py 2006 2009 BEER quarter 2008Q3 200601
 python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY month 200805 200601
-python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 2008Q2 200601
-python GenerateDataDidWithoutMktShare.py 2006 2009 GUM month 200805 200601
-python GenerateDataDidWithoutMktShare.py 2006 2009 GUM quarter 2008Q2 200601
+# python GenerateDataDidWithoutMktShare.py 2006 2009 CANDY quarter 2008Q2 200601
+# python GenerateDataDidWithoutMktShare.py 2006 2009 GUM month 200805 200601
+# python GenerateDataDidWithoutMktShare.py 2006 2009 GUM quarter 2008Q2 200601
