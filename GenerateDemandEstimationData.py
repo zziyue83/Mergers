d78a89ac66dc21c860202ec1073fac1a6b2fdadc
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-16 23:17:45 -0500

    nested logit fixed effects product_ids city_ids

--- GenerateDemandEstimationData.py
+++ GenerateDemandEstimationData.py
@@ -129,7 +129,7 @@ def TestGenerateDEData(product, frequency, inputs, characteristics, start, end):
     demand_estimation_data['nesting_ids'] = 1
     groups = demand_estimation_data.groupby(['market_ids', 'nesting_ids'])
     demand_estimation_data['demand_instruments'+str(len(inputs)+1)] = groups['shares'].transform(np.size)
-    nl_formulation = pyblp.Formulation(formulation, absorb='C(product_ids) + C(market_ids) + C(city_ids)')
+    nl_formulation = pyblp.Formulation(formulation, absorb='C(product_ids) + C(city_ids)')
     problem = pyblp.Problem(nl_formulation, demand_estimation_data)
     nlresults = problem.solve(rho=0.7)
     print(nlresults)
