a122e3e9701d90d7d2f34ade6eab5c84d1ccc059
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-28 18:26:16 -0500

    change variable names

--- ShareInfo.py
+++ ShareInfo.py
@@ -7,15 +7,21 @@ def ShareInfo(product,frequency):
     data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
     share_info = data[['dma_code_'+frequency,'market_share']]
     markets = share_info.groupby('dma_code_'+frequency)
+
     median = markets.median()
+    median = median.rename({'market_share':'median'})
     print(median)
     min = markets.min()
+    min = min.rename({'market_share':'min'})
     print(min)
     max = markets.max()
+    max = max.rename({'market_share':'max'})
     print(max)
     mean = markets.mean()
+    mean = mean.rename({'market_share':'mean'})
     print(mean)
     sum = markets.sum()
+    sum = sum.rename({'market_share':'sum'})
     print(sum)
 
 ShareInfo('BEER','month')
