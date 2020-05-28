7438adc4f7a62eb85961785beb5fb29ab309f2e8
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-28 15:17:27 -0500

    share mean

--- ShareInfo.py
+++ ShareInfo.py
@@ -4,5 +4,9 @@ def ShareInfo(product,frequency):
     data = pd.read_csv("../../GeneratedData/" + product + '_'+ frequency + "_pre_model_with_distance.tsv", delimiter = '\t')
     print(data.columns)
     print(data.head())
+    data['dma_code_'+frequency] = data['dma_code'].astype(str)+data[frequency].astype(str)
+    share_info = data[['dma_code_'+frequency,'market_share']]
+    mean = share_info.groupby('dma_code_'+frequency).mean()
+    print(mean)
 
 ShareInfo('BEER','month')
