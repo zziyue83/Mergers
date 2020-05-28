69ff33b18589c210f96f9ef47bae3e18d6e43986
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-05-28 15:08:54 -0500

    bugs

--- ShareInfo.py
+++ ShareInfo.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def ShareInfo(product,frequency):
     data = pd.read_csv("../../GeneratedData/" + product + '_'+ frequency + "_pre_model_with_distance.tsv", delimiter = '\t')
+    print(data.columns)
     print(data.head())
 
 ShareInfo('BEER','month')
