03ff2ae8bc7fd49fb74aa43cfd9e9471579b10ee
Aisling Chen <zcw7438@quser22.quest.it.northwestern.edu>
2020-06-20 09:47:21 -0500

    06/19

--- Main/submit_zcw7438.sh
+++ Main/submit_zcw7438.sh
@@ -4,10 +4,10 @@
 #SBATCH -p buyin
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -c 1
-#SBATCH -t 1:00:00
+#SBATCH -t 100:00:00
 #SBATCH --mail-user=ziyuechen2022@u.northwestern.edu
 #SBATCH --mem=0
 #SBATCH --nodes=1
 
 module load python/anaconda3.6
-python select_products.py 1912896020_1
+python select_products.py 1973045020_1
