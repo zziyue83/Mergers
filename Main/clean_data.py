b5f169f698d217b726d3e8078ce010bc139eec34
David Stillerman <dstillerman5@gmail.com>
2020-06-20 08:07:00 -0500

    Wrigley-Mars data error

--- Main/clean_data.py
+++ Main/clean_data.py
@@ -1,9 +1,12 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 def clean_data(code, df):
 	if code == '2600981020_1':
 		df.loc[df.multi * df.size1_amount == 1986 & df.size1_units == 'OZ', 'size1_units'] = 'ML'
 		df.loc[df.multi * df.size1_amount == 2130 & df.size1_units == 'OZ', 'size1_units'] = 'ML'
+
+	if code == '1973045020_1':
+		df.loc[df.multi * df.size1_amount == 87 & df.size1_units == 'OZ', 'size1_amount'] = .87
 
 	return df
