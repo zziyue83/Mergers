b6148b6f4893cb3f78f4d45d44b8a7a15b58e704
Yintian Zhan <yintianzhan2021@u.northwestern.edu>
2020-06-22 16:20:24 -0500

    quest push

--- Main/submit_twf0152~.sh
+++ Main/submit_twf0152~.sh
@@ -4,13 +4,11 @@
 #SBATCH -p buyin
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -c 1
-#SBATCH -t 1:00:00
-#SBATCH --mail-user=josesalas2025@u.northwestern.edu
+#SBATCH -t 12:00:00
+#SBATCH --mail-user=tianshiwang2022@u.northwestern.edu
 #SBATCH --mem=15G
 #SBATCH --nodes=1
 
 module load python/anaconda3.6
-
 cd /projects/b1048/gillanes/Mergers/Codes/Mergers/Main
-python get_units.py 2975652020_1
-
+python get_units.py 2105747040_1
