ef7946edd8c63dd91305e2ba3cd4376276ccf75d
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-06-22 02:19:38 -0500

    bugs

--- Main/compute_did.py
+++ Main/compute_did.py
@@ -50,14 +50,11 @@ def add_dhhi(df, merging_date, month_or_quarter):
 
 	# First, create shares for pre-merger period at the DMA level
 	df_pre = df.loc[(df['year'] < merger_year) | ((df['year'] == merger_year) & (df[month_or_quarter] < merger_month_or_quarter))].copy()
-	print(df_pre.columns)
-	df_pre = df_pre.groupby(['upc','dma_code'])['shares','owner'].agg('sum').reset_index()
-	print(df_pre.columns)
+	df_pre = df_pre.groupby(['upc','dma_code'])['shares'].agg('sum').reset_index()
 	df_pre['dma_share'] = df_pre.groupby('dma_code')['shares'].transform('sum') # We may want to generalize this. Right now, it assumes that market size is constant over time.
-	print(df_pre.columns)
 	df_pre['inside_share'] = df_pre['shares']/df_pre['dma_share']
 	print(df_pre.columns)
-	df_pre = df_pre[['upc','dma_code','inside_share','owner']]
+	df_pre = df_pre[['upc','dma_code','inside_share']]
 	df_pre = df_pre.rename(columns = {'inside_share' : 'shares'})
 
 	if merger_month_or_quarter > 1:
