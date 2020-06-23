0afb900db9b3dbbcf27eb8cca27f1eb7aafaaeb0
X1-Carbon\Vivek <bhattacharya.v@gmail.com>
2020-06-23 13:00:55 -0400

    added pickles

--- Main/clean_data.py
+++ Main/clean_data.py
@@ -3,14 +3,22 @@ import numpy as np
 
 def clean_data(code, df):
 	if code == '2600981020_1':
+		# Large sizes of liquor should be in ML, not OZ
 		df.loc[(df.multi * df.size1_amount == 1986) & (df.size1_units == 'OZ'), 'size1_units'] = 'ML'
 		df.loc[(df.multi * df.size1_amount == 2130) & (df.size1_units == 'OZ'), 'size1_units'] = 'ML'
 
-	if code == '1973045020_1':
+	elif code == '1973045020_1':
+		# These are small Pez dispensers
 		df.loc[(df.multi * df.size1_amount == 87) & (df.size1_units == 'OZ'), 'size1_amount'] = .87
 
 
-	if code == '2614332020_1':
+	elif code == '2614332020_1':
+		# These should be counts of cigarettes
 		df.loc[(df.multi * df.size1_amount == 200) & (df.size1_units == 'OZ'), 'size1_units'] = 'CT'	
+
+	elif code == '2736521020_1':
+		# These are pickle-in-a-pouch-type things (not comparable to 1 jar)
+		df.loc[df.size1_units == 'CT', 'size1_amount'] = df.size1_amount[df.size1_units == 'CT'] * 3.75
+		df.loc[df.size1_units == 'CT', 'size1_units'] = 'OZ'
 
 	return df
