8ba717b48ccee039cf4fdaed9519e72de6998184
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-06-26 01:43:53 -0500

    bugs

--- Main/auxiliary.py
+++ Main/auxiliary.py
@@ -184,6 +184,10 @@ def append_owners(code, df, month_or_quarter,add_dhhi = False):
 			brand_to_owner_test['overlap'] = overlap_or_gap
 			print(brand_to_owner_test[brand_to_owner_test['overlap'] != 0][['start_date_test_1','end_date_test_1','start_date_test_2','end_date_test_2','start_date_test_3','end_date_test_3']])
 			print(brand_to_owner_test[brand_to_owner_test['overlap'] != 0].index)
+			print(brand_to_owner_test[582868])
+			print(brand_to_owner_test[594982])
+			print(brand_to_owner_test[606506])
+			print(brand_to_owner_test[666355])
 			raise Exception('There are gaps or overlap in the ownership mapping.')
 
 	# Merge on brand and date intervals
