b8da10728e9d9c7cdedb117d3f110d16a8e3e94a
YintianZhan <yintianzhan2021@u.northwestern.edu>
2020-06-27 15:51:32 -0500

    dt

--- track_progress.py
+++ track_progress.py
@@ -53,7 +53,7 @@ def track_progress(base_folder):
 				progress['step9'][-1] = 'complete'
 
 	df = pd.DataFrame.from_dict(progress)
-	df = df.sort_values(by = 'RA').reset_index()
+	df = df.sort_values(by = 'RA').reset_index().drop('index', axis=1)
 	df.to_csv('progress.csv', sep = ',')
 
 base_folder = '../../All/'
