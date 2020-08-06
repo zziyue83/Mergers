5e4738129f41d246eea9ba157748103220f5745d
jdsalasm <jdsalas@uc.cl>
2020-08-06 13:12:38 -0500

    check overlap working

--- Sandbox/agg_test.py
+++ Sandbox/agg_test.py
@@ -4,6 +4,25 @@ import pandas as pd
 import auxiliary as aux
 import numpy as np
 
+def check_overlap(merger_folder):
+
+	overlap_file = pd.read_csv(merger_folder + '/overlap.csv', sep=',')
+	merging_sum = overlap_file['merging_party'].sum()
+
+	if merging_sum < 2:
+		return False
+
+	elif merging_sum ==3:
+		return True
+
+	elif merging_sum == 2:
+		df_merging = overlap_file[overlap_file['merging_party'] == 1]
+
+		if ((df_merging.loc[0,'pre_share'] == 0) & (df_merging.loc[1,'post_share'] == 0)) | ((df_merging.loc[0,'post_share'] == 0) & (df_merging.loc[1,'pre_share'] == 0)):
+			return False
+
+		else:
+			return True
 
 def get_betas(base_folder):
 
@@ -23,11 +42,11 @@ def get_betas(base_folder):
 		no_overlap_ls = []
 
 		#go inside folders with step5 finished
-		if (len(folder) > 10 and folder[0] == 'm') and (os.path.exists(merger_folder + '/did_stata_month_0.csv')):
+		if (os.path.exists(merger_folder + '/did_stata_month_0.csv')) and check_overlap(merger_folder):
 
-			overlap_csv = pd.read_csv(base_folder + folder + '/output/overlap.csv')
-			no_overlap_ls = check_overlap(overlap_csv, folder, no_overlap_ls)
-			print(no_overlap_ls)
+			#overlap_csv = pd.read_csv(base_folder + folder + '/output/overlap.csv')
+			#no_overlap_ls = check_overlap(overlap_csv, folder, no_overlap_ls)
+			#print(no_overlap_ls)
 
 			did_merger = pd.read_csv(merger_folder + '/did_stata_month_0.csv', sep=',')
 			did_merger.index = did_merger['Unnamed: 0']
@@ -51,25 +70,21 @@ def get_betas(base_folder):
 					aggregated['post_merger_merging'+'_'+i].append(did_merger[i]['post_merger_merging'])
 
 
+
 	df = pd.DataFrame.from_dict(aggregated)
 	df = df.sort_values(by = 'merger').reset_index().drop('index', axis=1)
 	df = aux.clean_betas(df)
-	df = df[~df['merger'].isin(no_overlap_ls)]
+#	df = df[~df['merger'].isin(no_overlap_ls)]
 
 
 	df.to_csv('aggregated.csv', sep = ',')
 
-def check_overlap(df, folder, no_overlap_ls):
-	merging_sum = np.sum(df['merging_party'])
-	print(merging_sum)
-	if merging_sum < 2:
-		no_overlap_ls.append(folder)
-	elif merging_sum == 2:
-		df_merging = df[df['merging_party'] == 1]
-		if ((df_merging.loc[0,'pre_share'] == 0) & (df_merging.loc[1,'post_share'] == 0)) | ((df_merging.loc[0,'post_share'] == 0) & (df_merging.loc[1,'pre_share'] == 0)):
-			no_overlap_ls.append(folder)
-	return no_overlap_ls
+
 
 
 base_folder = '../../../All/'
 get_betas(base_folder)
+
+
+
+
