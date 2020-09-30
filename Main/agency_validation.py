32c5e95d1cc998bebc15d1adf1a9f3949640a62e
David Stillerman <dstillerman5@gmail.com>
2020-09-30 17:32:55 -0500

    Debug

--- Main/agency_validation.py
+++ Main/agency_validation.py
@@ -97,17 +97,17 @@ def compute_nodivest_dhhi_agg(df, code, merging_date, merging_parties, volume):
 		df_pre = df_pre.rename(columns = {'inside_share_sales' : 'shares'})
 
 	# Compute pre-period HHI
-	df_pre_dma_owner['shares2'] = df_pre_dma_owner['shares'] * df_pre_dma_owner['shares']
-	hhi_pre = df_pre_dma_owner['shares2'].agg({'shares2':'sum'})
+	df_pre['shares2'] = df_pre['shares'] * df_pre['shares']
+	hhi_pre = df_pre['shares2'].agg({'shares2':'sum'})
 	hhi_pre = hhi_pre.rename(columns = {'shares2' : 'hhi_pre'})
 	hhi_pre['merger_code'] = code
 
 	# Add merging party indicator
-	df_post_dma_owner = dma_pre_dma_owner.copy()
-	df_post_dma_owner.loc[df_post_dma_owner['owner'].isin(merging_parties),'owner'] = 'MergedEntity'
-	df_post_dma_owner = df_post_dma_owner.groupby(['owner'])['shares'].agg({'shares':'sum'}).reset_index()
-	df_post_dma_owner['shares2'] = df_post_dma_owner['shares'] * df_pre_dma_owner['shares']
-	hhi_post = df_post_dma_owner['shares2'].agg({'shares2':'sum'})
+	df_post = dma_pre.copy()
+	df_post.loc[df_post['owner'].isin(merging_parties),'owner'] = 'MergedEntity'
+	df_post = df_post.groupby(['owner'])['shares'].agg({'shares':'sum'}).reset_index()
+	df_post['shares2'] = df_post['shares'] * df_post['shares']
+	hhi_post = df_post['shares2'].agg({'shares2':'sum'})
 	hhi_post = hhi_post.rename(columns = {'shares2' : 'hhi_post'})
 	hhi_post['merger_code'] = code
 
