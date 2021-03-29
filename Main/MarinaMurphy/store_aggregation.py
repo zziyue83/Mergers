7af1b72f517b032ffcf93828153f0e144c120f35
marinasiqueira2022 <marinasiqueira2022@u.northwestern.edu>
2021-03-29 16:23:16 -0300

    fixing code

--- Main/MarinaMurphy/store_aggregation.py
+++ Main/MarinaMurphy/store_aggregation.py
@@ -15,8 +15,8 @@ info_dict = aux.parse_info(code)
 groups, modules = aux.get_groups_and_modules(info_dict["MarketDefinition"])
 years = aux.get_years(info_dict["DateAnnounced"], info_dict["DateCompleted"])
 
-conversion_map = spm.get_conversion_map(code, info_dict["FinalUnits"])
-area_month_upc = spm.aggregate_movement(code, years, groups, modules, "month", conversion_map, info_dict["DateAnnounced"], info_dict["DateCompleted"])
+conversion_map = spm.get_conversion_map('2641303020_8', info_dict["FinalUnits"])
+area_month_upc = spm.aggregate_movement('2641303020_8', years, groups, modules, "month", conversion_map, info_dict["DateAnnounced"], info_dict["DateCompleted"])
 
 conversion_map.to_csv('conversion_map.csv')
 aggregate_movement.to_csv('aggregate_movement_removed_store_uc.csv')
