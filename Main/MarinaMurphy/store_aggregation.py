a840ae8d570d764514e11407d672693c95245b7d
marinasiqueira2022 <marinasiqueira2022@u.northwestern.edu>
2021-03-29 16:14:41 -0300

    select products

--- Main/MarinaMurphy/store_aggregation.py
+++ Main/MarinaMurphy/store_aggregation.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from clean_data import clean_data
+import select_products_marina as spm
 
 code = '2641303020_8'
 
@@ -14,8 +15,8 @@ info_dict = aux.parse_info(code)
 groups, modules = aux.get_groups_and_modules(info_dict["MarketDefinition"])
 years = aux.get_years(info_dict["DateAnnounced"], info_dict["DateCompleted"])
 
-conversion_map = get_conversion_map(code, info_dict["FinalUnits"])
-area_month_upc = aggregate_movement(code, years, groups, modules, "month", conversion_map, info_dict["DateAnnounced"], info_dict["DateCompleted"])
+conversion_map = spm.get_conversion_map(code, info_dict["FinalUnits"])
+area_month_upc = spm.aggregate_movement(code, years, groups, modules, "month", conversion_map, info_dict["DateAnnounced"], info_dict["DateCompleted"])
 
 conversion_map.to_csv('conversion_map.csv')
 aggregate_movement.to_csv('aggregate_movement_removed_store_uc.csv')
