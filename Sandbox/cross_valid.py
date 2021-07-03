5063862855f9d59cbc77823ade7f88b63d83c1e4
jdsalasm <jdsalas@uc.cl>
2021-07-03 14:52:36 -0500

    parallel compatible

--- Sandbox/cross_valid.py
+++ Sandbox/cross_valid.py
@@ -5,36 +5,37 @@ import sys
 import pandas as pd
 import unicodecsv as csv
 import auxiliary as aux
-from datetime import datetime
 
 
-def CV(base_folder, month_or_quarter='month'):
+def CV(folder, month_or_quarter='month'):
 
-    for folder in os.listdir(base_folder):
+        merger_folder = folder + '/output'
 
-        merger_folder = base_folder + folder + '/output'
+        print('Calling Stata: ' + folder)
+        code = folder[15:]
+        info_dict = aux.parse_info(code)
+        year = info_dict['DateCompleted'][0:4]
+        month = str(int(info_dict['DateCompleted'][5:7]))
 
-        if os.path.exists(merger_folder + '/demand_results_month.txt') and (not (os.path.exists(merger_folder + '/tables/r_18_3.csv'))):
+        if not os.path.isdir(folder + '/output/tables'):
+            os.makedirs(folder + '/output/tables')
 
-            print(folder)
-            code = folder[2:]
-            info_dict = aux.parse_info(code)
-            year = info_dict['DateCompleted'][0:4]
-            month = str(int(info_dict['DateCompleted'][5:7]))
-            if not os.path.isdir('../../../All/m_' + code + '/output/tables'):
-                os.makedirs('../../../All/m_' + code + '/output/tables')
-
-            dofile = "/projects/b1048/gillanes/Mergers/Codes/Mergers/Sandbox/cross_valid.do"
-            DEFAULT_STATA_EXECUTABLE = "/software/Stata/stata14/stata-mp"
-            path_input = "../../../All/m_" + code + "/intermediate"
-            path_output = "../output"
-            cmd = [DEFAULT_STATA_EXECUTABLE, "-b", "do", dofile, path_input, path_output, year, month]
-            subprocess.call(cmd)
+        dofile = "/projects/b1048/gillanes/Mergers/Codes/Mergers/Sandbox/cross_valid.do"
+        DEFAULT_STATA_EXECUTABLE = "/software/Stata/stata14/stata-mp"
+        path_input = folder + "/intermediate"
+        path_output = "../output"
+        cmd = [DEFAULT_STATA_EXECUTABLE, "-b", "do", dofile, path_input, path_output, year, month]
+        subprocess.call(cmd)
 
 
-log_out = open('output/CV_All.log', 'w')
-log_err = open('output/CV_All.err', 'w')
+folder = sys.argv[1]
+log_out = open('output/CV_All.log', 'a')
+log_err = open('output/CV_All.err', 'a')
 sys.stdout = log_out
 sys.stderr = log_err
 base_folder = '../../../All/'
-CV(base_folder)
+
+print(folder)
+
+if os.path.exists(folder + '/output/demand_results_month.txt'):
+    CV(folder)
